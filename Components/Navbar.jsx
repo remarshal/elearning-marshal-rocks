df598cb8b32edbe170e2703d4ef2986631cbf40a
remarshal <marshalwaldendev@gmail.com>
2022-05-28 18:23:41 -0400

    added icons

--- Components/Navbar.jsx
+++ Components/Navbar.jsx
@@ -1,22 +1,23 @@
 import { Menu } from 'antd';
 import Link from 'next/link';
+import { LoginOutlined, AppstoreOutlined, UserAddOutlined } from '@ant-design/icons';
 
 const { Item } = Menu;
 
 const Navbar = () => {
     return (
-        <Menu>
-            <Item>
+        <Menu mode='horizontal'>
+            <Item icon={<AppstoreOutlined />}>
                 <Link href="/">
                     <a>App</a>
                 </Link>
             </Item>
-            <Item>
+            <Item icon={<LoginOutlined />}>
                 <Link href="/login">
                     <a>Login</a>
                 </Link>
             </Item>
-            <Item>
+            <Item icon={<UserAddOutlined />}>
                 <Link href="/register">
                     <a>Register</a>
                 </Link>
