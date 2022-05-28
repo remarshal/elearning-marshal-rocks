bad1f9e6a45c6115e6e691ecfad35b7bee32c47c
remarshal <marshalwaldendev@gmail.com>
2022-05-28 17:45:53 -0400

    added menu items

--- Components/Navbar.jsx
+++ Components/Navbar.jsx
@@ -1,9 +1,27 @@
+import { Menu } from 'antd';
+import Link from 'next/link';
+
+const { Item } = Menu;
 
 const Navbar = () => {
     return (
-        <div>
-            <p>Navbar</p>
-        </div>
+        <Menu>
+            <Item>
+                <Link href="/">
+                    <a>App</a>
+                </Link>
+            </Item>
+            <Item>
+                <Link href="/login">
+                    <a>Login</a>
+                </Link>
+            </Item>
+            <Item>
+                <Link href="/register">
+                    <a>Register</a>
+                </Link>
+            </Item>
+        </Menu>
     );
 };
 
